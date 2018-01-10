d2646751f28d59b3472e6e21313460194e069cf2
sbpinilla <sbpinilla@hotmail.com>
2018-01-09 21:30:22 -0500

    Commit inicial

---
+++ ViewLoader/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  ViewLoader
+//
+//  Created by Sergio on 9/01/18.
+//  Copyright © 2018 sergio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cargarbtnClick(_ sender: UIButton) {
+        let sv = UIViewController.displaySpinner(onView: self.view)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 8) {
+          
+            UIViewController.removeSpinner(spinner: sv)
+            
+        }
+        
+       
+        
+        //
+        
+        
+    }
+}
+
+extension UIViewController {
+    class func displaySpinner(onView : UIView) -> UIView {
+        let spinnerView = UIView.init(frame: onView.bounds)
+        spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+        let ai = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
+        ai.startAnimating()
+        ai.center = spinnerView.center
+        
+        DispatchQueue.main.async {
+            spinnerView.addSubview(ai)
+            onView.addSubview(spinnerView)
+        }
+        
+        return spinnerView
+    }
+    
+    class func removeSpinner(spinner :UIView) {
+        DispatchQueue.main.async {
+            spinner.removeFromSuperview()
+        }
+    }
+}
+
